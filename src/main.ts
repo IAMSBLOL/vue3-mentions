f0837516060fb50925c92b24876ea65ee9acd9ce
IAMSBLOL <173537077@qq.com>
2024-03-15 17:29:51 +0800

    modify:lint

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import 'ant-design-vue/dist/reset.css';
+import 'ant-design-vue/dist/reset.css'
 import './assets/main.css'
-import Antd from 'ant-design-vue';
+import Antd from 'ant-design-vue'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
